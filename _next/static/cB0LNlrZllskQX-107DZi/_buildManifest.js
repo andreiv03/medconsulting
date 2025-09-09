d62aef3a1d82701a17a57fb2fd0eed9b3b272434
Andrei Voicu <andrei.voicu@resonate-mp4.com>
2025-09-09 09:51:47 +0300

    Updates

---
+++ _next/static/cB0LNlrZllskQX-107DZi/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/c8e886035c4a20f3.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/medconsulting//_next/:path+",
+        "destination": "/medconsulting/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/c89431215f44c427.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/a8eb3d4cf42e825f.js",
+  "static/chunks/2bb1f23e685ab327.js",
+  "static/chunks/turbopack-27b34f847377ea79.js"
+])
